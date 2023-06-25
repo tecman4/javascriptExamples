4da5fbac3e2b69a93d8fb38d9a9e80be9aaf366c
Wayne Bonner <tecman4@gmail.com>
2023-06-25 18:44:43 -0400

    Did some examples

---
+++ javascript.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!");
+
+
+let hello = 'Hello world!';
+
+let a=10
+let b = 7 * a
+alert(b)
+// copy 'Hello world' from hello into message
+message = hello;
+
+// now two variables hold the same data
+
+alert(message); // Hello world!
+
